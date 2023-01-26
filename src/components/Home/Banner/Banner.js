823307d8f569e381c6db16f4e8240de0406d2af0
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-01-26 21:29:33 +0600

    overview section done

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
       <div className='flex flex-col lg:flex-row justify-between items-center gap-5'>
         <div className='basis-1/2 text-center lg:text-start'>
           <h2 className='text-white text-3xl lg:text-5xl font-bold lg:leading-[52px]'>Learn to Code Blockchain DApps by Building Simple Games</h2>
-          <p className='text-[#c8c8c8] py-8 text-[18px] font-[500]'>CryptoZombies is an interactive school that teaches you all things technical about blockchains. Learn to write smart contracts by making your own crypto-collectibles game.</p>
+          <p className='text-[#c8c8c8] py-8 text-lg font-[500] '>CryptoZombies is an interactive school that teaches you all things technical about blockchains. Learn to write smart contracts by making your own crypto-collectibles game.</p>
           <div className="zombieBtn">
             <button className=''><Link to='/courses'>Courses</Link></button>
           </div>
--- src/components/Home/OverView/OverView.js
+++ src/components/Home/OverView/OverView.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import students from '../../../assets/students.png'
 
 const OverView = () => {
   return (
-    <div>OverView</div>
+    <div className='container mx-auto section__padding text-white'>
+      <div className="flex flex-col lg:flex-row gap-5 lg:items-center">
+
+        {/************************** Overview Left *************************** */}
+        <div className='basis-1/2 mt-10 '>
+          <h2 className='text-white text-center lg:text-start text-3xl lg:text-5xl font-bold lg:leading-[56px]'>Why CryptoZombies <br className='hidden lg:block' /> is Different</h2>
+
+          <div className='flex flex-col md:flex-row py-8 gap-5 justify-evenly items-center text-center lg:text-start'>
+            <div className='lg:basis-1/2'>
+              <img src={students} alt="students" />
+              <p className='p__style'>Students</p>
+            </div>
+            <div className='lg:basis-1/2'>
+              <h2 className='text-4xl font-bold'>1M+</h2>
+              <p className='p__style'>Classes Completed</p>
+            </div>
+          </div>
+        </div>
+
+        {/************************** Overview Right *************************** */}
+        <div className='lg:basis-1/2 text-center lg:text-start'>
+          <p className='p__style'>CryptoZombies is the largest education platform for blockchain development, it’s been around for 4+ years, with over 400k registered users that have finished multiple courses.</p>
+          <p className='p__style mt-6'>CryptoZombies was the first tutorial on the internet for NFTs, and is still very relevant to the new crop of web3 developers looking to enter the industry today. Currently the CryptoZombies curriculum is mostly focused on Ethereum and Solidity development, but is a lot of demand for content targeting other chains such as Binance, TRON, and even projects like Chainlink.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
