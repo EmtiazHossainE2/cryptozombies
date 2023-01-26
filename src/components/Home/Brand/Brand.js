1aa26af029a342c4788f59f54b2aa80473c04739
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-01-26 23:05:10 +0600

    brand section done

--- src/components/Home/Brand/Brand.js
+++ src/components/Home/Brand/Brand.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
+import alchemy from '../../../assets/brand/alchemy.png'
+import Chainlink from '../../../assets/brand/Chainlink.png'
+import Binance from '../../../assets/brand/Binance.png'
+import neo from '../../../assets/brand/neo.png'
+
+import './Brand.css'
+
+
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className="container mx-auto section__padding">
+      <div className="brand">
+        <div><img src={alchemy} alt="alchemy" /></div>
+        <div><img src={Chainlink} alt="Chainlink" /></div>
+        <div><img src={Binance} alt="Binance" /></div>
+        <div><img src={neo} alt="neo" /></div>
+      </div>
+    </div>
   )
 }
 
